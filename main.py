456b2a34a9adb59d17bbd7b7d16f5912f5abd157
Jubyy <zywicafilip08@gmail.com>
2024-01-10 19:22:56 +0100

    That's text to morse code simple console app and my first commit

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
-english_alphabet=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','0','1','2','3','4','5','6','7','8','9','.',',',':','?','!','/','-','@']
-morse_alphabet=[".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", "-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", ".-.-.-", "--..--", "---...", "..--..", "-.-.--", "-..-.", "-....-", "-..-."]
+english_alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+                    'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', ',', ':', '?',
+                    '!', '/', '-', '@']
+morse_alphabet = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.",
+                  "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", "-----",
+                  ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", ".-.-.-", "--..--",
+                  "---...", "..--..", "-.-.--", "-..-.", "-....-", "-..-."]
 
 
-wynik=''
-print('Write some text in English and this program will give you a Morse code . is short signal, - is long signal and space is " / " ')
-word=input('Please give some text: ')
+def text_to_morse(input_alphabet: list, morse_output: list):
+    final = ''
+    print(
+        'Write some text in Provided alphabet (Default is English) and this program will give you a Morse code . is '
+        'short signal, - is long signal'
+        'and space is " / " ')
+    word = input('Please give some text: ')
+    for one_char in word:
+        if one_char == ' ':
+            final += ' / '
+        else:
+            en_char = one_char.upper()
+            en_alphabet_index = input_alphabet.index(en_char)
+            morse_char = morse_output[en_alphabet_index]
+            final += f'{morse_char} '
+    print(final)
 
-for one_char in word:
-    if one_char==' ':
-        wynik+=' / '
-    else:
-        en_char=one_char.upper()
-        en_alphabet_index=english_alphabet.index(en_char)
-        morse_char=morse_alphabet[en_alphabet_index]
-        wynik+=f'{morse_char} '
-print(wynik)
 
-#s
+try:
+    text_to_morse(english_alphabet, morse_alphabet)
+except ValueError:
+    print('\nThere is no characters like this in your alphabet! Try again!!\n')
+    try:
+        text_to_morse(english_alphabet, morse_alphabet)
+    except ValueError:
+        print('\nTry again!!\n')
+
